65fdd799c7ab498dfb07f8e71ff65380e1ef810d
Mike <mikemilonakis7@gmail.com>
2015-09-23 19:01:04 +0300

    Bug fix on RandomizedQueue

--- randomized_queue_deque/src/RandomizedQueue.java
+++ randomized_queue_deque/src/RandomizedQueue.java
@@ -1,10 +1,11 @@
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import edu.princeton.cs.algs4.StdRandom;
 
 
-public class RandomizedQueue<Item> {
-    
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
     private int index = 0;
     private Item[] queue;
     
@@ -24,39 +25,39 @@ public class RandomizedQueue<Item> {
         if (item == null)
             throw new NullPointerException();
         if (index == queue.length)
-            resizeAndShuffle(2 * queue.length);
+            resizeArray(2 * queue.length);
         queue[index++] = item;
     }
     
     public Item dequeue() {
         if (queue[0] == null)
             throw new NoSuchElementException();
-        Item item = queue[--index];
-        queue[index] = null;
+        int randomIndex = StdRandom.uniform(index);
+        Item item = queue[randomIndex];
+        queue[randomIndex] = queue[--index];
         if (index > 0 && index == queue.length / 4)
-            resizeAndShuffle(queue.length/2);
+            resizeArray(queue.length/2);
         return item;
     }
     
+    public Item sample() {
+        if (queue[0] == null)
+            throw new NoSuchElementException();
+        return queue[StdRandom.uniform(index)];
+    }
     
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
     
-    private void resizeAndShuffle(int capacity) {
+    private void resizeArray(int capacity) {
         Item[] copy = (Item[]) new Object[capacity];
         for (int i = 0; i < index; i++) {
-            int r = StdRandom.uniform(i+1);
-            exchange(queue, i, r);
             copy[i] = queue[i];
         }
         queue = copy;
         
     }
-//
-//    private void shuffle(Item[] queue) {
-//        for (int i=0; i<index; i++) {
-//            int r = StdRandom.uniform(i+1);
-//            exchange(queue, i, r);
-//        }
-//    }
 
     private void exchange(Object[] array, int i, int r) {
         Object temp = array[i];
@@ -65,5 +66,69 @@ public class RandomizedQueue<Item> {
     }
     
     
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+//        queue.enqueue(7);
+//        queue.enqueue(8);
+//        queue.enqueue(9);
+//        
+        for (Integer integer : queue) {
+            System.out.println(integer);
+        }
+//        
+//        for (Integer integer : queue) {
+//            System.out.println(integer);
+//        }
+//        
+//        for (Integer integer : queue) {
+//            System.out.println(integer);     
+//        }
+//        
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+    }
+    
+    
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        
+        private final Item[] array;
+        private int current = 0;
 
+        private RandomizedQueueIterator() {
+            array = (Item[]) new Object[index];
+            for (int i = 0; i < index; i++) {
+                array[i] = queue[i];
+                int r = StdRandom.uniform(i+1);
+                exchange(array, i, r);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (current != array.length);
+        }
+
+        @Override
+        public Item next() {
+            if (hasNext())
+                return array[current++];
+            throw new NoSuchElementException();
+        }
+        
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
