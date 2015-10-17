2769cdaacdd6b0d89a33459a2c026feb80313637
Mike <mikemilonakis7@gmail.com>
2015-10-17 11:38:50 +0300

    Added gitignore file

--- pattern_recognition/src/FastCollinearPoints.java
+++ pattern_recognition/src/FastCollinearPoints.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import edu.princeton.cs.algs4.StdOut;
-
 public class FastCollinearPoints {
 
     private ArrayList<LineSegment> lineSegments;
 
     public FastCollinearPoints(Point[] points) {
+        if (points == null)
+            throw new NullPointerException();
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new NullPointerException();
+        }
         this.lineSegments = new ArrayList<LineSegment>();
         for (int i = 0; i < points.length; i++) {
             Comparator<Point> pointComparator = points[i].slopeOrder();
@@ -21,75 +25,47 @@ public class FastCollinearPoints {
             double slope = -1;
             int collinears = 1;
             boolean lineAlreadyAdded = false;
-            StdOut.println("---------------------------------------------------------------------");
-            StdOut.println("i: " + i + " points[i]: " + points[i]);
             for (int j = 0; j < sortedSlopes.length; j++) {
-                StdOut.println("j: " + j + " sortedSlope[j] " + sortedSlopes[j]
-                        + " points[i].slopeTo(sort[j]: "
-                        + points[i].slopeTo(sortedSlopes[j]));
-                if (slope != -1) {
-                    if (points[i].slopeTo(sortedSlopes[j]) == slope) {
-                        collinears++;
-                        if (sortedSlopes[j].compareTo(max) >= 0)
-                            max = sortedSlopes[j];
-                        else if (sortedSlopes[j].compareTo(min) < 0)
-                            min = sortedSlopes[j];
-                        StdOut.println("In While loop, j = " + j
-                                + "  , collinears = " + collinears);
-                        if (j != sortedSlopes.length - 1)
-                            continue;
-                    }
-                    if (collinears >= 4) {
-                        StdOut.println("Added Segment: Min: " + min + "  Max: "
-                                + max);
-                        for (int k = i-1; k>=0; k--) {
-                            if (points[i].slopeTo(points[k]) == slope) {
-                                //We have already added this Line
-                                lineAlreadyAdded = true;
-                            }
-                        }
-                        if (!lineAlreadyAdded)
-                            lineSegments.add(new LineSegment(min, max));
-                        slope = points[i].slopeTo(sortedSlopes[j]);
-                        collinears = 2;
-                        if (sortedSlopes[j].compareTo(max) >= 0) {
-                            max = sortedSlopes[j];
-                            min = points[i];
-                        }
-                        else if (sortedSlopes[j].compareTo(min) < 0) {
-                            min = sortedSlopes[j];
-                            max = points[i];
-                        }
-                        else
-                            min = max = points[i];
-                    } else {
-                        if (sortedSlopes[j].compareTo(max) >= 0) {
-                            max = sortedSlopes[j];
-                            min = points[i];
-                        }
-                        else if (sortedSlopes[j].compareTo(min) < 0) {
-                            min = sortedSlopes[j];
-                            max = points[i];
-                        }
-                        else
-                            min = max = points[i];
-                        slope = points[i].slopeTo(sortedSlopes[j]);
-                        collinears = 2;
-                    }
-                } else {
-                    collinears += 1;
-                    slope = points[i].slopeTo(sortedSlopes[j]);
-                    if (sortedSlopes[j].compareTo(max) >= 0) {
+                if (slope == Double.NEGATIVE_INFINITY)
+                    throw new IllegalArgumentException();
+                if (points[i].slopeTo(sortedSlopes[j]) == slope) {
+                    collinears++;
+                    if (sortedSlopes[j].compareTo(max) >= 0)
                         max = sortedSlopes[j];
-                        min = points[i];
-                    }
-                    else if (sortedSlopes[j].compareTo(min) < 0) {
+                    else if (sortedSlopes[j].compareTo(min) < 0)
                         min = sortedSlopes[j];
-                        max = points[i];
+                    if (j != sortedSlopes.length - 1)
+                        continue;
+                }
+                if (collinears >= 4) {
+                    for (int k = i - 1; k >= 0; k--) {
+                        if (points[k].slopeTo(max) == slope) {
+                            // We have already added this Line
+                            lineAlreadyAdded = true;
+                        }
                     }
+                    if (!lineAlreadyAdded)
+                        lineSegments.add(new LineSegment(min, max));
+                }
+                lineAlreadyAdded = false;
+                slope = points[i].slopeTo(sortedSlopes[j]);
+                collinears = 2;
+                if (sortedSlopes[j].compareTo(points[i]) >= 0) {
+                    max = sortedSlopes[j];
+                    min = points[i];
+                } else if (sortedSlopes[j].compareTo(points[i]) < 0) {
+                    min = sortedSlopes[j];
+                    max = points[i];
+                } else {
+                    min = points[i];
+                    max = points[i];
                 }
             }
         }
+    }
+
+    public int numberOfSegments() {
+        return lineSegments.size();
     }
 
     public LineSegment[] segments() {
